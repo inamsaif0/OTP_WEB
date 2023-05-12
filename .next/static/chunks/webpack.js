708ad24dd847810542f554f8a9bd9e6f258d5477
inamsaif0 <inamsaif586@gmail.com>
2023-05-12 02:53:20 -0700

    feat: changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d25f5c3b8b96ab7"; }
+/******/ 		__webpack_require__.h = function() { return "0ada21945664d0bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
