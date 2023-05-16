42ed63e66f20f462fc87a7383ed567b7da84e45d
inamsaif0 <inamsaif586@gmail.com>
2023-05-15 19:28:32 -0700

    feat: adding build

--- pages/users/createUser.js
+++ pages/users/createUser.js
@@ -7,9 +7,9 @@ export default function Dashboard(){
         <Layout>
             
                 
-        
+            
             <Stack flexDirection={'column'} alignItems='center' justifyContent='flex-start' gap='2rem' lg="2" sm="2" md="2" sx={{width:'50%', mt:{lg:'4rem',md:'2rem',sm:'4rem'}, ml:{lg:'15rem', md:'2rem', sm:'1rem'}}}>
-            <CreateUserForm level="Level" value="Create User"/>
+            <CreateUserForm level="Level" title="Create User"/>
             </Stack>
         </Layout>
 
